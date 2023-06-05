b61d852ccfe39b74ec5e121dd1142706233ea865
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-06-04 23:35:42 -0300

    feat: adicionando o contexto no container de dependencias

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using APICatalogo.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<Context>(
+  options =>
+  //Dizendo que vamos usar o MySQL
+  options.UseMySql(
+      //Pegando as configurações de acesso ao BD
+      builder.Configuration.GetConnectionString("DefaultConnection"),
+      //Detectando o Servidor de BD
+      ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+  )
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
