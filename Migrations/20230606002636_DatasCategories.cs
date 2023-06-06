0c52626c465459429bed1f8f3d8532c7811e9b66
matheuz-siqueira <matheusbatista.dev@gmail.com>
2023-06-05 22:14:46 -0300

    feat: adicionando dados de teste

---
+++ Migrations/20230606002636_DatasCategories.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    public partial class DatasCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder
+                .Sql("Insert into Categories(Name, ImageUrl) Values('Bebidas', 'bebidas.jpg')");            
+
+            migrationBuilder
+                .Sql("Insert into Categories(Name, ImageUrl) Values('Lanches', 'lanches.jpg')");
+
+            migrationBuilder
+                .Sql("Insert into Categories(Name, ImageUrl) Values('Sobremesas', 'sobremesas.jpg')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categories");
+        }
+    }
+}
